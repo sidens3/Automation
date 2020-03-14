fd67ddd960298058416460e653d29d3d99af6259
sidens3 <sidens3@gmail.com>
2020-03-14 18:17:07 +0300

    add tests for tab location on the page

--- Тестовое «Инфосистемы Джет»/src/main/java/YandexSearchPageTest.java
+++ Тестовое «Инфосистемы Джет»/src/main/java/YandexSearchPageTest.java
@@ -40,7 +40,7 @@ public class YandexSearchPageTest {
     public void testGetLipetskFirstLine() {
         String searchWord = "липецк";
         setWordOnYandexSearchField("липецк");
-        String output =getStringBySelector("body .mini-suggest__popup-content [data-index='0']");
+        String output = getStringBySelector("body .mini-suggest__popup-content [data-index='0']");
         System.out.println(output);
         assert output.contains(searchWord);
     }
@@ -74,6 +74,52 @@ public class YandexSearchPageTest {
         String output = getStringBySelector("body .mini-suggest__popup-content [data-index='0']");
         System.out.println(output);
         assert output.contains(searchWord);
+    }
+
+    @Test // вкладка Картанки отображается на странице
+    public void testTabImagesIsDisplayed() {
+        driver.get(baseUrl);
+        assert driver.findElement(By.cssSelector("body .home-arrow__tabs [data-id=\"images\"]")).isDisplayed();
+    }
+
+    @Test //вкладка Картинки располагается на нужном месте
+    public void testCorrectLocationTabImages(){
+        driver.get(baseUrl);
+        Point tabImagesPoint = driver.findElement(By.cssSelector("body .home-arrow__tabs [data-id=\"images\"]")).getLocation();
+        System.out.println(tabImagesPoint.toString());
+        assert (tabImagesPoint.x == 247) && (tabImagesPoint.y == 244);
+    }
+
+    @Test // вкладка Картанки имеет соответствующее название
+    public void testCorrectNameTabImages() {
+        driver.get(baseUrl);
+        assert driver.findElement(By.cssSelector("body .home-arrow__tabs [data-id=\"images\"]")).getText().equals("Картинки");
+    }
+
+    @Test  //вкладка Картанки имеет нужный размер
+    public void testSizeTabImages() {
+        driver.get(baseUrl);
+        Integer imagesTabHeight =  driver.findElement(By.cssSelector("body .home-arrow__tabs [data-id=\"images\"]")).getSize().getHeight();
+        Integer imagesTabWidth =  driver.findElement(By.cssSelector("body .home-arrow__tabs [data-id=\"images\"]")).getSize().getWidth();
+        assert (imagesTabHeight == 17) && (imagesTabWidth == 64);
+    }
+
+    @Test //вкладка Картанки находится между вкладками видео и новости
+    public void testTabImagesBetweenVideoAndNews() {
+        driver.get(baseUrl);
+        Integer tabVideoLocationX = driver.findElement(By.cssSelector("body .home-arrow__tabs [data-id=\"video\"]")).getLocation().getX();
+        Integer tabImagesLocationX = driver.findElement(By.cssSelector("body .home-arrow__tabs [data-id=\"images\"]")).getLocation().getX();
+        Integer tabNewsLocationX = driver.findElement(By.cssSelector("body .home-arrow__tabs [data-id=\"news\"]")).getLocation().getX();
+        assert (tabVideoLocationX < tabImagesLocationX) && (tabImagesLocationX < tabNewsLocationX);
+
+    }
+
+    @Test //вкладка Картанки находится над полем поиск
+    public void testTabImagesUnderSearchField() {
+        driver.get(baseUrl);
+        Integer tabImagesLocationY = driver.findElement(By.cssSelector("body .home-arrow__tabs [data-id=\"images\"]")).getLocation().getY();
+        Integer tabSearchFieldLocationY =   driver.findElement(By.id("text")).getLocation().getY();
+        assert tabImagesLocationY < tabSearchFieldLocationY;
     }
 
     @After
